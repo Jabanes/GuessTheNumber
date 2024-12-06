8f3200e48a832cc657181dd8b1211721a6c3964c
Your NameJabanes <erezhabani2003@gmail.com>
2024-12-06 12:59:31 +0200

    Added input validation

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
 
-
-
-
-
-randomNUm = Math.floor(Math.random()*100)+1
-console.log(randomNUm);
+randomNUm = Math.floor(Math.random() * 100) + 1
+console.log(randomNUm);7
 
 Guesses = 1
 
 
-function submit(){
+function submit() {
 
     document.getElementById("Guesses").textContent = `Number of Guesses: ${Guesses}`
     Guess = document.getElementById("inputNumber").value;
     Guess = parseInt(Guess)
     document.getElementById("Guess").textContent = `Your Guess is: ${Guess}`
-    
-    
-        if (Guess < randomNUm){
-            console.log(`Too low`);
-            Guesses += 1
-            
-        }
 
-        if (Guess > randomNUm){
-            console.log(`Too High`);
-            Guesses += 1
-        
-        }
-        if (Guess == randomNUm){
-            console.log(`Correct!`);
-          
-        }
-        
+    if (isNaN(Guess)) {
+        document.getElementById("Guess").textContent = `Please type in a valid number!`
+        document.getElementById("Guesses").textContent = ``
+
     }
+    else{
+
+        if (Guess < randomNUm) {
+            document.getElementById("Guess").textContent = `${Guess} is Too Low!`
+            Guesses += 1
     
+        }
+    
+        if (Guess > randomNUm) {
+            document.getElementById("Guess").textContent = `${Guess} is Too High!`
+            Guesses += 1
+    
+        }
+        if (Guess == randomNUm) {
+            document.getElementById("Guess").textContent = `CORRECT! the number was: ${randomNUm}`
+            document.getElementById("Guesses").textContent = `It took you ${Guesses} Guesses`
+        }
+    
+    }
+
+    
+}
+
 
 
 
